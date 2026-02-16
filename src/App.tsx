24d85c01f174014ded058a291abb8f2176b06c03
joselito3500 <jpendon.jimenez@gmail.com>
2026-02-16 09:32:14 +0100

    Corrigiendo errores de compilación y nombres de archivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes,HashRouter,Navigate, Route } from 'react-router-dom'
 import Navbar from './components/navbar'
 import Projects from './components/Projects'
 import About from './components/About'
@@ -13,7 +13,7 @@ const Home = () => (
     <p className="text-slate-400 mt-4">Navega por las secciones para conocerme mejor.</p>
   </div>
 )
-
+/*
 const App: React.FC = () => {
   return (
     <Router>
@@ -25,6 +25,7 @@ const App: React.FC = () => {
           <Route path="/proyectos" element={<Projects />} />
           <Route path="/sobre-mi" element={<About />} />
           <Route path="/contacto" element={<Contact />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
 
         <Footer />
@@ -33,6 +34,34 @@ const App: React.FC = () => {
     
   )
 }
+*/
 
+function App() {
+  return (
+    <HashRouter>
+      {/* Contenedor principal que ocupa toda la pantalla */}
+      <div className="flex flex-col min-h-screen bg-slate-950">
+        <Navbar />
+        
+        {/* El contenido crece y empuja al footer hacia abajo */}
+        <main className="flex-grow">
+          <Routes>
+            {/* Ruta principal */}
+            <Route path="/" element={<Home />} />
+            
+            {/* Tus otras rutas */}
+            <Route path="/sobre-mi" element={<About />} />
+            <Route path="/proyectos" element={<Projects />} />
+
+            {/* Redirección de seguridad: si entran a una ruta que no existe, al Inicio */}
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </HashRouter>
+  );
+}
 export default App
 
