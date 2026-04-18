4618e946833a59e28cb76af3f7db3b0341302f52
joselito3500 <jpendon.jimenez@gmail.com>
2026-04-18 06:01:57 +0200

    colores

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,7 +11,7 @@ const Navbar: React.FC = () => {
   
 
   return (
-    <nav className="fixed top-0 left-0 w-full bg-slate-900/90 backdrop-blur-md border-b border-slate-800 z-50">
+    <nav className="fixed top-0 left-0 w-full bg-mist-800/90 backdrop-blur-md border-b border-mist-700 z-50">
       <div className="max-w-7xl mx-auto px-6 h-20 flex items-center justify-between">
         
         {/* Logo */}
